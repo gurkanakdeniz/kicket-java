a0ed4139a0b94452c49db6360c315d39f61cdbc5
gurkanakdeniz <akdenizgurkan@gmail.com>
2019-08-19 20:58:06 +0300

    example comment review

--- src/main/java/com/kicket/api/kicketapi/response/ExampleResponse.java
+++ src/main/java/com/kicket/api/kicketapi/response/ExampleResponse.java
@@ -6,7 +6,7 @@ public class ExampleResponse extends BaseResponse {
     private static String exampleRequest;
 
     static {
-        exampleCode = "/* You can define multiple method definition, just change <ex> keyword with your method name. */\n /* If you changed <default> keyword, you need to send your method name in request */\n/* ex. { method: <your method name> } */\n" +
+        exampleCode = "/* You can define multiple method definition, just change <ex> keyword with your method name. */\n /* If you changed <ex> keyword, you need to send your method name in request */\n/* ex. { method: <your method name> } */\n" +
                 "public class External {\n" +
                 "    \n" +
                 "    public static String ex(Object[] args) {\n" +
